0b9837cb1a780921da04871ea9c8d3b5f0f971e0
ealhl <yayt.tuk@gmail.com>
2023-12-25 02:25:46 -0800

    update username

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const generateRandomString = () => {
   let result = "";
@@ -38,7 +40,8 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
